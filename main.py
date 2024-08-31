d7c5d7b19160a702340e62efaa9357820efa7d41
USER <none>
2024-08-31 12:04:56 +0800

    add templates and books.html

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 # print(__name__)
 
 app = Flask(__name__)
 
-books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}
 
 
 @app.route("/")
 def index():
+    name = "Jerry"
     today = datetime.now().strftime("%Y-%m-%d %H:%M")
     print(today)
-    return f"<h1>Hello Flask!</h1><h2>Hi Flask!</h2><h3>How are you Flask?</h3><h4>Flask {today}!</h4>"
+    # return f"<h1>Hello Flask!</h1><h2>Hi Flask!</h2><h3>How are you Flask?</h3><h4>Flask {today}!</h4>"
+    return render_template("index.html", date=today, name=name)
 
 
 @app.route("/books")
 def showBooks():
-    return books
+    for key in books:
+        print(books[key])
+    return render_template("books.html", books=books)
 
 
 @app.route("/book/<int:id>")
